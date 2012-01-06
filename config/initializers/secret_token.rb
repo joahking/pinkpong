66c952769c455b472ec0763b33c1630f248b5cf6
Joaquin Rivera Padron <joahking@gmail.com>
2012-01-06 17:47:07 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinkpong::Application.config.secret_token = '876608e71247fb33020a375ffbc3a141e254ff540d4136c6d6883511855964063ec1bf30cdbbe1648757b447a90bda8ab71c32062e9ff82832f37fe12837604c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pinkpong::Application.config.session_store :cookie_store, :key => '_pinkpong_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pinkpong::Application.config.session_store :active_record_store
