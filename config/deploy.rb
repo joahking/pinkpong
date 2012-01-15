a9682a4f3c91a44f8e6cb866aac2aab1551d8079
Joaquin Rivera Padron <joahking@gmail.com>
2012-01-15 20:44:45 +0100

    [deploy] config deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,32 @@
-set :application, "pinkpong"
-set :repository,  "https://github.com/joahking/pinkpong.git"
+require 'bundler/capistrano'
 
+set :application, "pinkpong"
+set :domain, "pinkpong.pent.es"
+
+set :repository,  "https://github.com/joahking/pinkpong.git"
 set :scm, :git
-set :branch, 'master'
+set :branch, 'deploy'
 set :git_shallow_clone, 1
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "pinkpong.3scale.net"                          # Your HTTP server, Apache/etc
-role :app, app                          # This may be the same as your `Web` server
-role :db,  app, :primary => true # This is where Rails migrations will run
-role :db,  app
+set :use_sudo, false
+set :user, 'deploy'
+set :deploy_to, "/home/deploy/#{application}"
+
+set :shared_dirs, ['config', 'tmp', 'public/system']
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+role :db,  domain
+
+after "deploy:update_code", "update_code"
+
+desc "Make symlink for app yaml"
+task :update_code do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+end
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
